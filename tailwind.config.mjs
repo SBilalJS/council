fc3f2dbd1fa94d4c16e5b3d97308fd6ec04d3980
SBilalJS <sbilal.js@gmail.com>
2025-01-02 15:52:57 +0500

    [project created]

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,9 +7,35 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
+        play: ['Playwrite CO Guides', 'sans-serif']
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+    },
+    screens: {
+      'x-s': { max: '575px' }, 
+      's-m': { max: '767px' }, 
+      'm-d': { max: '991px' }, 
+      'l-g': { max: '1199px' }, 
+      'x-l': { max: '1399px' }, 
+      '2x-l': { min: '1400px' }, 
+    },
+    container: {
+      center: true, 
+      screens: {
+        'x-s': '100%',    
+        's-m': '540px',   
+        'm-d': '720px',   
+        'l-g': '960px',   
+        'x-l': '1140px',  
+        '2x-l': '1200px',
       },
     },
   },
